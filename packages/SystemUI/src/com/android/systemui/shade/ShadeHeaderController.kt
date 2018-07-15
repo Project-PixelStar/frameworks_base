f12cd0a0a20ff93fbf41f1cd98c29a9853d4ab9c
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 17:09:54 +0200

    base: Status bar clock customizations [1/2] SQUASH of the following commits:
    SystemUI: Add tunables for clock position
    
    SystemUI: Add tunables for clock AM/PM style
    
    frameworks: Add support for clock auto-hiding
    
    SystemUI: Disable clock auto hide feature for QS
    
    Add separate tunable for clock seconds
    
    Statusbar clock customizations
    
    Add option to auto hide status-bar clock
    
    Use position tunable to hide clock as well
    
    SystemUI: Properly apply dark theming to clock
    
    SystemUI: Retune clock paddings
    
    SystemUI: Fix height for center and right clock
    
    SystemUI: Block few clock customizations in QS header
    
    SystemUI: Fix up status bar end side content layout
    
    Clock: Use executor for tuner settings
    
    Co-authored-by: althafvly <althafvly@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -331,6 +331,8 @@ constructor(
         }
         clock.setOnClickListener { launchClockActivity() }
 
+        clock.setQsHeader()
+
         dumpManager.registerDumpable(this)
         configurationController.addCallback(configurationControllerListener)
         demoModeController.addCallback(demoModeReceiver)
