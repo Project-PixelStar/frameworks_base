00e18edd7ac2bd32fb30321fa342fb9d9b514c4e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-28 12:51:07 -0300

    base: Allow changing face unlock method when locked [1/2]
    Change-Id: I9a8e812bbe869dead94d631fa0a2f53f3bf67cc6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12801,6 +12801,12 @@ public final class Settings {
         public static final String SPOOF_STORAGE_ENCRYPTION_STATUS = "spoof_storage_encryption_status";
         
         /**
+         * Face Unlock Method
+         * @hide
+         */
+        public static final String FACE_UNLOCK_METHOD = "face_unlock_method";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
