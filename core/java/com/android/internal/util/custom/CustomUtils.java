a3812030467207dc5f320f63943385d90e6b288f
jhonboy121 <alfredmathew05@gmail.com>
2022-02-01 19:54:15 +0530

    base: Introduce app lock [1/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I19ed5ed8638776b0e93ecf8a308936030f9a9f92
    
    -----
    
    base: applock: refactor
    
    * encapsulate config data sources and add a data class for per app config
    * added support for migrating between different versions of data
    * fixed notification content not being redacted.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    services: applock: allow background authentication
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I1d05263f562d8ca4ac5d53c260b89814b399e0ea
    
    -----
    
    base: merge hidden app feature with applock
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Ie1966d856710353c849fa6bd26732dccea0ada85
    
    -----
    
    services: AppLockConfig: fix a derp in hidePackage()
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    services: applock: fix data migration and change secure notification parameter name
    
    * also fixed addPackage replacing data without checking if entry already exists
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    services: AppLockManagerService: use the og calling uid for target intent
    
    * fixes launching app activites not exported
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    AppLock: Allow locking all apps visible in launcher [1/2]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I920885d11aa9a74ca835f1d026d0834f9c851df9
    
    -----
    
    AppLock: Update API
    
    * To allow hiding apps in launcher even if app not protected
    
    Change-Id: I5e77b75be4d5aa0d137b04a15bc2f41cbc32a22d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    AppLock: Fix hidden apps reset after reboot
    
    Change-Id: I2dadc340f6a9c00aefe4ee9b8dc074a1c8aca717
    
    [aswin7469]
    - Adapted for 13 QPR3
    
    [Dhina17]
    - Adapted for android 14.
    
    [Pranav Vashi]
    - Add privapp permissions for Settings, updated for A14
    
    Change-Id: I906286e573fce744328edd29f11d0785c07c95a2
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    
    -----
    
    AppLock: Move services to new package
    
    Change-Id: Iffb96348d15e71e89a246326275f1b6f88b4e103
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/custom/CustomUtils.java
+++ core/java/com/android/internal/util/custom/CustomUtils.java
@@ -21,6 +21,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -29,6 +30,9 @@ import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomUtils {
 
@@ -70,5 +74,23 @@ public class CustomUtils {
 
     public static boolean isPackageInstalled(Context context, String pkg) {
         return isPackageInstalled(context, pkg, true);
+    }
+
+    public static List<String> launchablePackages(Context context) {
+        List<String> list = new ArrayList<>();
+
+        Intent filter = new Intent(Intent.ACTION_MAIN, null);
+        filter.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> apps = context.getPackageManager().queryIntentActivities(filter,
+                PackageManager.GET_META_DATA);
+
+        int numPackages = apps.size();
+        for (int i = 0; i < numPackages; i++) {
+            ResolveInfo app = apps.get(i);
+            list.add(app.activityInfo.packageName);
+        }
+
+        return list;
     }
 }
