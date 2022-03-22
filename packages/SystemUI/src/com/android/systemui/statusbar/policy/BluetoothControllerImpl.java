08a75d29f5008f54ae4493482d433419fda9baf6
jhonboy121 <alfredmathew05@gmail.com>
2022-03-22 19:15:24 +0530

    SystemUI: Fetch bluetooth battery level from any device
    * When multiple devices are connected and only one or a few supports battery level reports, instead of fetching level from the first device, loop through the list to try and find any device that reports battery level, or just return unsupported value constant if there isn't any
    
    Change-Id: I2542ad683b1c37fccd88a3cc666409d41ec0097f
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothControllerImpl.java
@@ -19,6 +19,7 @@ package com.android.systemui.statusbar.policy;
 import android.annotation.Nullable;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.os.Handler;
