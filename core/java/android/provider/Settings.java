f811a0115574686456c6a12c96f13ef188c8d8e9
Pranav Vashi <neobuddy89@gmail.com>
2022-04-22 20:23:15 +0530

    base: Optional haptic feedback on back gesture [1/2]
    This is inspired from
    https://github.com/syberia-project/platform_frameworks_base/commit/2a57b330c1f697ef23460047bd62f6dd20f3368a
    
    However, we do not want to ditch AOSP implementation.
    
    Change-Id: I365717428a68845878f144e8a6f661d94b1713a6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    base: Add optional haptic feedback to new back gesture affordance
    
    Change-Id: Ie0c95c3ab0da250fd00bff7b593ce69dda550567
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12461,6 +12461,12 @@ public final class Settings {
          */
         public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
 
+        /** 
+         * Whether or not to vibrate when back gesture is used
+         * @hide
+         */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
