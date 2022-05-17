010184c9d1b586f805b5a63cf8fe79bad5318de0
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-17 23:20:38 +0800

    reTicker: Do not show when content/icon is null
    Change-Id: I4ece6be5afe4e51f0506768563dc1277fdb0f4d1

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -60,6 +60,7 @@ import android.app.StatusBarManager;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.ContentObserver;
 import android.graphics.Color;
 import android.graphics.ColorMatrixColorFilter;
@@ -78,6 +79,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.VibrationEffect;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionListenerAdapter;
@@ -5300,10 +5302,14 @@ public final class NotificationPanelViewController implements Dumpable {
                 } else {
                     icon = mView.getContext().getPackageManager().getApplicationIcon(pkgname);
                 }
-            } catch (android.content.pm.PackageManager.NameNotFoundException e) {
+            } catch (NameNotFoundException e) {
+                return;
             }
-            if (row.getEntry().getSbn().getNotification().extras.getString("android.text") != null) {
-                reTickerContent = row.getEntry().getSbn().getNotification().extras.getString("android.text");
+            String content = row.getEntry().getSbn().getNotification().extras.getString("android.text");
+            if (!TextUtils.isEmpty(content)) {
+                reTickerContent = content;
+            } else {
+                return;
             }
             String reTickerAppName = row.getEntry().getSbn().getNotification().extras.getString("android.title");
             PendingIntent reTickerIntent = row.getEntry().getSbn().getNotification().contentIntent;
@@ -5317,9 +5323,9 @@ public final class NotificationPanelViewController implements Dumpable {
                 col = row.getEntry().getSbn().getNotification().color;
                 mAppExceptions = mView.getContext().getResources().getStringArray(R.array.app_exceptions);
                 //check if we need to override the color
-                for (int i=0; i < mAppExceptions.length; i++) {
+                for (int i = 0; i < mAppExceptions.length; i++) {
                     if (mAppExceptions[i].contains(pkgname)) {
-                        col = Color.parseColor(mAppExceptions[i+=1]);
+                        col = Color.parseColor(mAppExceptions[i += 1]);
                     }
                 }
                 dw.setTint(col);
