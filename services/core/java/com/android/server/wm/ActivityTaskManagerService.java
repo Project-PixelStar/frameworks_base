8bf0ec2aafa68346c55c1b780477e8b1ca229394
LibXZR <i@xzr.moe>
2022-06-11 16:13:53 +0800

    base: Add support for strict standby policy [SQUASH]
    When dismissing from recents, this will force stop apps with restricted
    battery usage and put them into RESTRICTED standby bucket. Also all
    restrictions will be imposed even when device is charging.
    
    This can be useful to kill dirty vigorous Chinese apps.
    
    [ghostrider-reborn]:
    - Add more restrictions (jobs, alarms)
    - Show toast only in ENG build
    - Remove settings toggle
    
    Change-Id: I8f075ef48c5775971e3e0b8b1ea843d70a2e44c4
    
     BroadcastQueue: Skip delivering broadcasts to restricted apps
    
    Fixes plenty of timeouts blocking the whole queue.
    
    Also strict standby is enhanced that nothing could wakeup a
    static broadcast receiver.
    
    mnri: adapted to 13
    Change-Id: I7b748a63287319e765d1548b28987f56be5e599e
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    [adapted to 14]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -288,6 +288,7 @@ import com.android.server.sdksandbox.SdkSandboxManagerLocal;
 import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
+import com.android.server.usage.AppStandbyInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
 
@@ -814,6 +815,8 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     private CutoutFullscreenController mCutoutFullscreenController;
 
+    public AppStandbyInternal mAppStandbyInternal;
+
     private final class SettingObserver extends ContentObserver {
         private final Uri mFontScaleUri = Settings.System.getUriFor(FONT_SCALE);
         private final Uri mHideErrorDialogsUri = Settings.Global.getUriFor(HIDE_ERROR_DIALOGS);
@@ -900,6 +903,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
             mGrammaticalManagerInternal = LocalServices.getService(
                     GrammaticalInflectionManagerInternal.class);
         }
+        mAppStandbyInternal = LocalServices.getService(AppStandbyInternal.class);
     }
 
     public void onInitPowerManagement() {
