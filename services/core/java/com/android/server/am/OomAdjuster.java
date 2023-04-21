818ce343cf48f7512f86e366e4835f5f98c4cf51
Andrzej Perczak <linux@andrzejperczak.com>
2023-04-22 00:27:27 +0200

    oom: Enable proactive kills only on modern kernels
    Proactive Kills feature helps with mm especially after long uptimes. I
    observed that zram usage goes up to 100% usage after a few days which
    causes unneded kills and worser multitasking. Proactive kills help
    mitigate this issue, however, this mechanism should be used only on
    modern kernel, ie. with in-kernel LMK disabled, PSI and MGLRU enabled.
    It was observed that on older devices kills are excessive and
    multitasking becomes worse.
    
    To fix this enable PK only on modern kernels, simple path checks should
    be enough to differentiate between supported features.
    
    Change-Id: I8d4c74a42e3b15264b2cc68b4ab455dbc0f867a2
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -166,6 +166,7 @@ import com.android.server.am.PlatformCompatCache.CachedCompatChangeId;
 import com.android.server.wm.ActivityServiceConnectionsHolder;
 import com.android.server.wm.WindowProcessController;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -520,6 +521,20 @@ public class OomAdjuster {
         mTmpQueue = new ArrayDeque<ProcessRecord>(mConstants.CUR_MAX_CACHED_PROCESSES << 1);
         mNumSlots = ((CACHED_APP_MAX_ADJ - CACHED_APP_MIN_ADJ + 1) >> 1)
                 / CACHED_APP_IMPORTANCE_LEVELS;
+
+        // Enable Proactive Kills on devices with modern kernel mm setup
+        conditionallyEnableProactiveKills();
+    }
+
+    void conditionallyEnableProactiveKills() {
+        File mglru = new File("/sys/kernel/mm/lru_gen/enabled");
+        File psi = new File("/proc/pressure/memory");
+        File lmk_kernel = new File("/sys/module/lowmemorykiller/parameters/minfree");
+
+        if (!lmk_kernel.exists() && mglru.exists() && psi.exists()) {
+            Slog.i(TAG, "Detected kernel with modern mm setup, enabling Proactive Kills.");
+            mProactiveKillsEnabled = true;
+        }
     }
 
     void initSettings() {
@@ -1291,6 +1306,8 @@ public class OomAdjuster {
         return CachedAppOptimizer.getFreeSwapPercent();
     }
 
+    private boolean mProactiveKillsEnabled = mConstants.PROACTIVE_KILLS_ENABLED;
+
     @GuardedBy({"mService", "mProcLock"})
     private void updateAndTrimProcessLSP(final long now, final long nowElapsed,
             final long oldTime, final ActiveUids activeUids, @OomAdjReason int oomAdjReason,
@@ -1316,9 +1333,8 @@ public class OomAdjuster {
         int numEmpty = 0;
         int numTrimming = 0;
 
-        boolean proactiveKillsEnabled = mConstants.PROACTIVE_KILLS_ENABLED;
         double lowSwapThresholdPercent = mConstants.LOW_SWAP_THRESHOLD_PERCENT;
-        double freeSwapPercent =  proactiveKillsEnabled ? getFreeSwapPercent() : 1.00;
+        double freeSwapPercent = mProactiveKillsEnabled ? getFreeSwapPercent() : 1.00;
         ProcessRecord lruCachedApp = null;
 
         for (int i = numLru - 1; i >= 0; i--) {
@@ -1366,7 +1382,7 @@ public class OomAdjuster {
                                     ApplicationExitInfo.REASON_OTHER,
                                     ApplicationExitInfo.SUBREASON_TOO_MANY_CACHED,
                                     true);
-                        } else if (proactiveKillsEnabled) {
+                        } else if (mProactiveKillsEnabled) {
                             lruCachedApp = app;
                         }
                         break;
@@ -1387,7 +1403,7 @@ public class OomAdjuster {
                                         ApplicationExitInfo.REASON_OTHER,
                                         ApplicationExitInfo.SUBREASON_TOO_MANY_EMPTY,
                                         true);
-                            } else if (proactiveKillsEnabled) {
+                            } else if (mProactiveKillsEnabled) {
                                 lruCachedApp = app;
                             }
                         }
