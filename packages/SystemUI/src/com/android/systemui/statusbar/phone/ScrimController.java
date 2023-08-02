13de0f214d57210861464ecbb990ae934db2dca7
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-02 08:07:07 +0800

    SystemUI: Hide notification scrim background when entering split shade Adapt it for dual-tone black/white qs
    Change-Id: I191b9e17d85931f029657abecf9ed648d11e19ae
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -25,6 +25,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.annotation.IntDef;
 import android.app.AlarmManager;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Trace;
@@ -256,6 +257,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     private boolean mWallpaperSupportsAmbientMode;
     private boolean mScreenOn;
     private boolean mTransparentScrimBackground;
+    private boolean mIsLandscape;
 
     // Scrim blanking callbacks
     private Runnable mPendingFrameCallback;
@@ -275,7 +277,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                 mScrimInFront.setViewAlpha(mInFrontAlpha);
 
                 mNotificationsAlpha = alphas.getNotificationsAlpha();
-                mNotificationsScrim.setViewAlpha(mNotificationsAlpha);
+                mNotificationsScrim.setViewAlpha(mIsLandscape ? 0 : mNotificationsAlpha);
+                if (mIsLandscape && mNotificationsScrim != null) {
+                    mNotificationsScrim.setVisibility(View.GONE);
+                }
 
                 mBehindAlpha = alphas.getBehindAlpha();
                 mScrimBehind.setViewAlpha(mBehindAlpha);
@@ -341,6 +346,13 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             public void onUiModeChanged() {
                 ScrimController.this.onThemeChanged();
             }
+
+            @Override
+            public void onConfigChanged(Configuration newConfig) {
+                int orientation = newConfig.orientation;
+                mIsLandscape = orientation == Configuration.ORIENTATION_LANDSCAPE;
+                ScrimController.this.onThemeChanged();
+            }
         });
         mColors = new GradientColors();
         mPrimaryBouncerToGoneTransitionViewModel = primaryBouncerToGoneTransitionViewModel;
@@ -360,6 +372,9 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
         behindScrim.enableBottomEdgeConcave(mClipsQsScrim);
         mNotificationsScrim.enableRoundedCorners(true);
+        if (mIsLandscape && mNotificationsScrim != null) {
+            mNotificationsScrim.setVisibility(View.GONE);
+        }
 
         if (mScrimBehindChangeRunnable != null) {
             mScrimBehind.setChangeRunnable(mScrimBehindChangeRunnable, mMainExecutor);
@@ -1133,7 +1148,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
         setScrimAlpha(mScrimInFront, mInFrontAlpha);
         setScrimAlpha(mScrimBehind, mBehindAlpha);
-        setScrimAlpha(mNotificationsScrim, mNotificationsAlpha);
+        setScrimAlpha(mNotificationsScrim, mIsLandscape ? 0 : mNotificationsAlpha);
 
         // The animation could have all already finished, let's call onFinished just in case
         onFinished(mState);
@@ -1274,7 +1289,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         } else if (scrim == mScrimBehind) {
             return mBehindAlpha;
         } else if (scrim == mNotificationsScrim) {
-            return mNotificationsAlpha;
+            return mIsLandscape? 0 : mNotificationsAlpha;
         } else {
             throw new IllegalArgumentException("Unknown scrim view");
         }
@@ -1347,7 +1362,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             mNotificationsTint = mState.getNotifTint();
             updateScrimColor(mScrimInFront, mInFrontAlpha, mInFrontTint);
             updateScrimColor(mScrimBehind, mBehindAlpha, mBehindTint);
-            updateScrimColor(mNotificationsScrim, mNotificationsAlpha, mNotificationsTint);
+            updateScrimColor(mNotificationsScrim, mIsLandscape ? 0 : mNotificationsAlpha, mNotificationsTint);
         }
     }
 
@@ -1468,14 +1483,17 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     }
 
     private void updateThemeColors() {
-        if (mScrimBehind == null) return;
+        if (mScrimBehind == null || mColors == null) return;
         int background = Utils.getColorAttr(mScrimBehind.getContext(),
                 android.R.attr.colorBackgroundFloating).getDefaultColor();
+        int mainColor = mIsLandscape ? background : background;
         int accent = Utils.getColorAccent(mScrimBehind.getContext()).getDefaultColor();
-        mColors.setMainColor(background);
+        mColors.setMainColor(mainColor);
         mColors.setSecondaryColor(accent);
         mColors.setSupportsDarkText(
                 ColorUtils.calculateContrast(mColors.getMainColor(), Color.WHITE) > 4.5);
+
+
         mNeedsDrawableColorUpdate = true;
     }
 
