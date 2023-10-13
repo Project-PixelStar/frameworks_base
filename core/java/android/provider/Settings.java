1d7484788bccc5e4e73ec4ee2e7fdae652d86ab9
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-13 11:34:43 +0800

    services:[SQUASH] Introduce Pocket Mode feature [1/2]
    reference used: https://github.com/minaripenguin/android_frameworks_base/commit/e3ccb262279f83e39e3935995b1d3a9178577d72
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: sanjheevi <sanjheevisv@palladiumos.org>
    
    Pocket mode: Do not persist if unlocked manually
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Pocket Mode: Implement idle timeout mechanism
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Pocket Mode: Improve motion detection
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Pocket Mode: Always reset unlock state during screen on/off
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    services: Avoid system crash when retrieving spatializer tracking mode
    
    --------- beginning of crash
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: AudioService
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke interface method 'byte android.media.ISpatializer.getActualHeadTrackingMode()' on a null object reference
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at com.android.server.audio.SpatializerHelper.createSpat(SpatializerHelper.java:960)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at com.android.server.audio.SpatializerHelper.setSpatializerEnabledInt(SpatializerHelper.java:794)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at com.android.server.audio.SpatializerHelper.reset(SpatializerHelper.java:335)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at com.android.server.audio.AudioService.onAudioServerDied(AudioService.java:1740)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at com.android.server.audio.AudioService$AudioHandler.handleMessage(AudioService.java:9144)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:205)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:294)
    10-21 16:36:42.132  1673  2225 E AndroidRuntime: 	at com.android.server.audio.AudioService$AudioSystemThread.run(AudioService.java:8943)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Pocket Mode: Prevent idle timeout from turning off display after user unlocks
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11776,6 +11776,17 @@ public final class Settings {
         public static final String LOCK_SCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
+         * Parameter for Pocket Mode
+         *
+         * The value "1", Enable Pocket Mode
+         * The value "0", Disables Pocket Mode
+         *
+         * @hide
+         */
+        public static final String POCKET_MODE_ENABLED
+                = "pocket_mode_enabled";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
