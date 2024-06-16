ffc946421f7567041a77e87c587d2d8538420b61
someone5678 <someone5678@users.noreply.github.com>
2024-06-16 23:09:52 +0900

    fixup! SystemUI: Add statusbar NFC icon
    Change-Id: Id09dd7ad9cbf9a16028a5eeda3eb48e62961156c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -427,7 +427,7 @@ public class PhoneStatusBarPolicy
     private NfcAdapter getAdapter() {
         if (mAdapter == null) {
             try {
-                mAdapter = NfcAdapter.getNfcAdapter(mContext);
+                mAdapter = NfcAdapter.getDefaultAdapter(mContext);
             } catch (UnsupportedOperationException e) {
                 mAdapter = null;
             }
