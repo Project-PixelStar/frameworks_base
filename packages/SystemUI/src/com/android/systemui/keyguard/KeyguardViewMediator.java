3fb1ba8e3f0be3e0624d9c0bf1d0fbb19d0ab129
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-11 08:59:40 +0800

    Delay garbage collection to prevent deadlocks during screen on
    test: turn screen on/off, no more black screen when showing lockscreen
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2829,12 +2829,6 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             userActivity();
             mUpdateMonitor.setKeyguardGoingAway(false);
             mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false);
-            final int oldMask = StrictMode.getThreadPolicyMask();
-            StrictMode.setThreadPolicyMask(0);
-            System.gc();
-            System.runFinalization();
-            System.gc();
-            StrictMode.setThreadPolicyMask(oldMask);
             if (mShowKeyguardWakeLock != null)
                 mShowKeyguardWakeLock.release();
         }
@@ -2842,6 +2836,16 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
         scheduleNonStrongBiometricIdleTimeout();
 
+        // Delay garbage collection until display is shown
+        mHandler.postDelayed(() -> {
+            final int oldMask = StrictMode.getThreadPolicyMask();
+            StrictMode.setThreadPolicyMask(0);
+            System.gc();
+            System.runFinalization();
+            System.gc();
+            StrictMode.setThreadPolicyMask(oldMask);
+        }, 2500);
+            
         Trace.endSection();
     }
 
