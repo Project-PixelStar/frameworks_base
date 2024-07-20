b4fdf952d417639928bdb8dc4d506a85b8d02905
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-20 20:11:34 +0800

    services: Ensure freeform tasks bounds gets updated when launching tasks
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -2877,6 +2877,12 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
                     mService.continueWindowLayout();
                 }
             }
+            if (activityOptions != null) {
+                final int windowingMode = activityOptions.getLaunchWindowingMode();
+                if (windowingMode == WINDOWING_MODE_FREEFORM) {
+                    task.setBounds(activityOptions.getLaunchBounds());
+                }
+            }
             taskCallingUid = task.mCallingUid;
             callingPackage = task.mCallingPackage;
             callingFeatureId = task.mCallingFeatureId;
