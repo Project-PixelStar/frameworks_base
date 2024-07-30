ac025f8359741e1e57251b610eec923df5b7525d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-31 01:09:48 +0800

    SystemUI: Optimize notification fade-in/out transition
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2795,6 +2795,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             if (mQsController.isExpandImmediate() && !mQsController.getFullyExpanded()) {
                 alpha = 0f;
             }
+            if (alpha < 0.95f) {
+                alpha = 0f;
+            }
             mNotificationStackScrollLayoutController.setMaxAlphaForExpansion(alpha);
         }
     }
