2f4696a06dd4c18a72d9d4505de67039256dffa0
chiteroman <querveros999@gmail.com>
2024-09-09 00:45:03 +0530

    Spoof a valid certificate chain for hardware attestations
    @neobuddy89: Move out of PixelPropsUtils.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Co-authored-by: chiteroman <querveros999@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -483,22 +483,6 @@ public final class PixelPropsUtils {
         return gmsUid == callingUid;
     }
 
-    private static boolean isCallerSafetyNet() {
-        return Arrays.stream(Thread.currentThread().getStackTrace())
-                        .anyMatch(elem -> elem.getClassName().toLowerCase()
-                            .contains("droidguard"));
-    }
-
-    public static void onEngineGetCertificateChain() {
-        if (!SystemProperties.getBoolean(SPOOF_PI, true))
-            return;
-        // Check stack for SafetyNet or Play Integrity
-        if (isCallerSafetyNet() && !sIsExcluded) {
-            dlog("Blocked key attestation");
-            throw new UnsupportedOperationException();
-        }
-    }
-
     public static void dlog(String msg) {
         if (DEBUG) Log.d(TAG, "[" + sProcessName + "] " + msg);
     }
